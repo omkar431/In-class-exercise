1569b7d4aec4fb5be0421b998555538118f97038
Omkar Modak <58703706+omkar431@users.noreply.github.com>
2020-02-07 12:19:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            car myObj = new car();
+            Console.WriteLine("Color of car is" + " " + myObj.color);
+            Console.WriteLine("Max speed of car is "+ " " + myObj.maxspeed);
+        }
+    }
+
+    class car
+    {
+        public string color = "black";
+        public int maxspeed = 200;
+    }
+}
+
+
+
